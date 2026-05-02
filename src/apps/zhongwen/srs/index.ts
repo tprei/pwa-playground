c033dc5b708c79ffa1feb30cf8ea5efab9535ca1
minion <minion@local>
2026-05-02 18:33:07 +0000

    Add ts-fsrs scheduler for zhongwen

--- src/apps/zhongwen/srs/index.ts
+++ src/apps/zhongwen/srs/index.ts
@@ -1,1 +1,11 @@
-export {};
+export { createScheduler, createSchedulerWith } from "./scheduler";
+export type { Scheduler } from "./scheduler";
+export { applyGrade, initialFsrsState } from "./fsrs";
+export {
+  defaultSettings,
+  readSettings,
+  writeSettings,
+  readCounters,
+  writeCounters,
+} from "./settings";
+export type { SrsSettings, DailyCounters } from "./settings";
